a7aa883a6a7827c71ab60f3edaabc3e533ec6ac3
ratbert <ratbert>
2009-12-25 09:25:28 +0000

    add test patterns

---
+++ test/test.sh
@@ -0,0 +1,21 @@
+#
+if [ $# != 1 ] || [ ! -d $1 ]
+then
+	echo "Usage: $0 <pattern dir>"
+	exit 1
+fi
+
+for file in $1/*
+do
+	if [ -f $file ]
+	then
+		cp $file /dev/fb0
+
+		sleep 1
+		for yoffset in 0 20 40 60 80 100 120 140 160 180 200 220 240 0
+		do
+			echo "0,$yoffset" > /sys/class/graphics/fb0/pan
+		done
+	fi
+done
+
