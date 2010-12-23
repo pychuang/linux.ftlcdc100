1072d46bbbbe09a476de3c239dfabe3f0aec6076
Po-Yu Chuang <ratbert@faraday-tech.com>
2010-12-23 11:34:04 +0800

    test script: use cat to copy files to /dev/fb0
    cp of newer version busybox overwrites /dev/fb0 in the following command line
    	cp file /dev/fb0
    
    use cat file > /dev/fb0 instead

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,6 @@
-#
+# in case we forgot to create device files
+mknod /dev/fb0 c 29 0
+
 if [ $# != 1 ] || [ ! -d $1 ]
 then
 	echo "Usage: $0 <pattern dir>"
@@ -9,7 +11,7 @@ for file in $1/*
 do
 	if [ -f $file ]
 	then
-		cp $file /dev/fb0
+		cat $file > /dev/fb0
 
 		sleep 1
 		for yoffset in 0 20 40 60 80 100 120 140 160 180 200 220 240 0
